18f384f37f25a1176f80be283604d1784294ba56
ManTiuss <virtual31777@gmail.com>
2023-06-24 16:35:32 +0300

    hello js

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Hello!");
+
+let answer = prompt("What's your name?", "");
+console. log(answer);
+
+let result = confirm("Hello, John! Are you feeling good?");
+if(result) {
+    console. log("Great, John! Glad to hear this!");
+} else {
+    console. log("Ough! It's sad to hear that. Hopefully, it won't be for long!");
+}
