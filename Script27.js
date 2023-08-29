689043f874fec58e691ce71a64899f79c53421d5
ManTiuss <virtual31777@gmail.com>
2023-08-29 11:08:17 +0300

    Script27

---
+++ Script27.js
@@ -0,0 +1,13 @@
+const table = document.createElement('table');
+let count = 1;
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement('tr');
+  for (let j = 0; j < 10; j++) {
+    const cell = document.createElement('td');
+    cell.textContent = count;
+    row.appendChild(cell);
+    count++;
+  }
+  table.appendChild(row);
+}
+document.body.appendChild(table);
