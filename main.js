d2caea71510052e80c5068a8c14335b9b4f4c09a
ManTiuss <virtual31777@gmail.com>
2023-09-13 20:29:42 +0300

    homeworking

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-alert("Hello!");
-
-let answer = prompt("What's your name?", "");
-console. log(answer);
-
-let result = confirm("Hello, "+ answer +  "\nAre you feeling good?");
-if(result) {
-    alert("Great "+ answer +  "\nGlad to hear this!");
-} else {
-    alert("Ough! \nIt's sad to hear that. \nHopefully, it won't be for long!");
-}
+function generateList(array) {
+    const result = document.createElement('ul');
+  
+    for (const item of array) {
+      const listItem = document.createElement('li');
+  
+      if (Array.isArray(item)) {
+        listItem.appendChild(generateList(item));
+      } else {
+        listItem.textContent = item;
+      }
+  
+      result.appendChild(listItem);
+    }
+  
+    return result;
+  }
+  
+  const inputArray = [1, 2, 3, [1, 2, 3], 4];
+  const outputElement = generateList(inputArray);
+  document.body.appendChild(outputElement);
